7265e031c827bef9a979efa1ce67e5e14fe4c591
Brittany Vipperman <b_vipperman@yahoo.com>
2019-09-28 22:08:35 -0500

    update landing page

--- SnapTacklePop/src/app/app.module.ts
+++ SnapTacklePop/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule

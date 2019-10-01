c5b6c3349ac54310d2762c0e12ab5043793b9919
Brittany Vipperman <b_vipperman@yahoo.com>
2019-09-30 21:13:45 -0500

    updated landingpage

--- SnapTacklePop/src/app/app.module.ts
+++ SnapTacklePop/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes:Routes=[
+  {path:''}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
